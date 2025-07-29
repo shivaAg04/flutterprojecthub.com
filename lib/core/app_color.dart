aff708d03476f0ac5c66bb4794294e4ffd419459
Shiva Agrahari <81668752+shivaAg04@users.noreply.github.com>
2025-07-29 12:22:40 +0530

    Update app_color.dart

--- lib/core/app_color.dart
+++ lib/core/app_color.dart
@@ -3,8 +3,9 @@ import 'dart:ui';
 class AppColor {
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF000000);
-
-  static const Color primaryColor = Color(0xFF03DAC6);
+ static const Color appBarBgColor = Color(0xFF060622);
+  // static const Color primaryColor = Color(0xFF03DAC6);
+  static const Color primaryColor =  Color(0xFF060622);
   static const Color background = Color(0xFFF5F5F5);
   static const Color error = Color(0xFFB00020);
   static const Color surface = Color(0xFF121212);
