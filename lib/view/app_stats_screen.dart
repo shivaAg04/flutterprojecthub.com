0c4c66709b8ef1254212d2dd194044f05da6d936
Shiva Agrahari <81668752+shivaAg04@users.noreply.github.com>
2025-07-29 21:04:57 +0530

    Update app_stats_screen.dart

--- lib/view/app_stats_screen.dart
+++ lib/view/app_stats_screen.dart
@@ -2,41 +2,44 @@ import 'package:flutter/material.dart';
 
 import '../core/app_color.dart';
 
-class AppStatsScreen extends StatelessWidget {
-  const AppStatsScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColor.primaryColor,
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: AppColor.white),
-          onPressed: () => Navigator.of(context).pop(),
+showFutureFeatureBottomSheet(BuildContext context) {
+  showModalBottomSheet(
+    context: context,
+    backgroundColor: AppColor.surface,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+    ),
+    builder: (context) {
+      return SafeArea(
+        child: Container(
+          color: AppColor.appBarBgColor,
+          child: Column(
+            children: [
+              Center(
+                child: TextWithPadding(
+                  text: 'Future Features',
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TextWithPadding(
+                        text: "1. Total number of apps listed on the platform"),
+                    TextWithPadding(text: "2. Event listings by location"),
+                    TextWithPadding(text: "3. Job openings section"),
+                    TextWithPadding(text: "4. Curated articles section"),
+                    TextWithPadding(text: "5. Apps categorized by industry"),
+                    TextWithPadding(text: "6. Flutter Package hub"),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
-        backgroundColor: AppColor.appBarBgColor,
-        centerTitle: true,
-        titleTextStyle: TextStyle(
-          color: AppColor.white,
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-        ),
-        title: Text('Future Features'),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          TextWithPadding(
-              text: "1. Total number of apps listed on the platform"),
-          TextWithPadding(text: "2. Event listings by location"),
-          TextWithPadding(text: "3. Job openings section"),
-          TextWithPadding(text: "4. Curated articles section"),
-          TextWithPadding(text: "5. Apps categorized by industry"),
-          TextWithPadding(text: "6. Flutter Package hub"),
-        ],
-      ),
-    );
-  }
+      );
+    },
+  );
 }
 
 class TextWithPadding extends StatelessWidget {
