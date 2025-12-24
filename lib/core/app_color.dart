5681f0df0e8d0e9b542381310883b947fa1bdf7b
Shiva Agrahari <81668752+shivaAg04@users.noreply.github.com>
2025-12-24 10:52:17 +0530

    Replace primaryColor with appBarBgColor value

--- lib/core/app_color.dart
+++ lib/core/app_color.dart
@@ -4,7 +4,8 @@ class AppColor {
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF000000);
  static const Color appBarBgColor = Color(0xFF060622);
-  // static const Color primaryColor = Color(0xFF03DAC6);
+  // static const Color primaryColor = Color(0xFF03DAC6)
+  
   static const Color primaryColor =  Color(0xFF060622);
   static const Color background = Color(0xFFF5F5F5);
   static const Color error = Color(0xFFB00020);
